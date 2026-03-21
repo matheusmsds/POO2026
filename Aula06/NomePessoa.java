369e39a983ed4259ef775053955c8a7307dc8550
matheusmsds <matheus.mendes.soaresdasilva@gmail.com>
2026-03-20 22:41:59 -0300

    Add files via upload

---
+++ Aula06/NomePessoa.java
@@ -0,0 +1,38 @@
+// mostrar o nome da pessoa com maior idade, usando classes.
+// usar o vetor para guardar as pessoas, e fazer um algoritmo que calcula a maior idade.
+
+class Pessoa2{
+    String nome;
+    int idade;
+
+    Pessoa2(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+
+public class NomePessoa {
+    public static void main(String[] args) {
+        Pessoa2 pessoas[] = new Pessoa2[3];
+        pessoas[0] = new Pessoa2("Ana", 15);
+        pessoas[1] = new Pessoa2("Bob", 18);
+        pessoas[2] = new Pessoa2("Carl", 16);
+
+        Pessoa2 result = maiorIdade(pessoas);
+        System.out.println("Pessoa com maior idade: ");
+        System.out.println(result);
+    }
+    public static void maiorIdade(Pessoa2[] vetor){
+        int maioridade = vetor[0];
+        String nome = vetor[0][1];
+
+        for(int i = 0; i < vetor.length; i++){
+            if (vetor[i][1] > maioridade) {
+                maioridade = vetor[i];
+                nome = vetor[i][1];
+            }
+        }
+        return nome;
+    }
+}
