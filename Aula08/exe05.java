09b941b9f78e825bdeeaaf318195b27ca4438ada
matheusmsds <matheus.mendes.soaresdasilva@gmail.com>
2026-04-10 21:29:39 -0300

    Fix variable initialization and correct loop logic

--- Aula08/exe05.java
+++ Aula08/exe05.java
@@ -1,29 +1,31 @@
+
 // este arquivo vai do exercico 12 até 14
 import java.util.Scanner;
-class Aluno{
+
+class Aluno {
     String nome;
     double nota;
 
-    Aluno(String nome, double notas){
+    Aluno(String nome, double notas) {
         this.nome = nome;
-        this.notas = nota;
+        this.nota = nota;
     }
 }
 
 public class exe05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Aluno [] vet = new Aluno[3];
+        Aluno[] vet = new Aluno[3];
 
-        for(int i = 0; i < vet.length; i++){
+        for (int i = 0; i < vet.length; i++) {
             vet[i].nome = scanner.nextLine();
             vet[i].nota = scanner.nextDouble();
         }
 
-        maiorNota = vet[0].nota;
-        nomeAluno = vet[0].nome;
+        double maiorNota = vet[0].nota;
+        String nomeAluno = vet[0].nome;
 
-        for(int i = 0; i < notas.length; i++){
+        for (int i = 0; i < vet.length; i++) {
             if (maiorNota < vet[i].nota) {
                 nomeAluno = vet[i].nome;
             }
